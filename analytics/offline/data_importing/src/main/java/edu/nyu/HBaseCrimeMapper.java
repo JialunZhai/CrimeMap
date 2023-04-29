d54e777d2d8d62db854f219a916476680128e82e
Jialun Zhai <jlzhai@ustc.edu>
2023-04-29 18:27:27 -0400

    fixed data_importing

--- analytics/offline/data_importing/src/main/java/edu/nyu/HBaseCrimeMapper.java
+++ analytics/offline/data_importing/src/main/java/edu/nyu/HBaseCrimeMapper.java
@@ -1,22 +1,22 @@
 import ch.hsr.geohash.GeoHash;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import org.apache.hadoop.hbase.KeyValue;
-import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class DataImportingMapper
-    extends Mapper<LongWritable, Text, ImmutableBytesWritable, KeyValue> {
-  final static byte[] COL_FAMILY = "e".getBytes();
-  ImmutableBytesWritable hKey = new ImmutableBytesWritable();
-  KeyValue kv;
+public class HBaseCrimeMapper<K> extends Mapper<LongWritable, Text, K, Put> {
+  static final byte[] EVENT_COLUMNFAMILY = Bytes.toBytes("e");
+  static final byte[] LONGITUDE_QUALIFIER = Bytes.toBytes("x");
+  static final byte[] LATITUDE_QUALIFIER = Bytes.toBytes("y");
+  static final byte[] TIME_QUALIFIER = Bytes.toBytes("t");
+  static final byte[] DESCRIPTION_QUALIFIER = Bytes.toBytes("d");
 
   private String normalize(double value, double minValue) {
-    double posValue = value + minValue;
+    double posValue = value - minValue;
     long prefixValue = (long) posValue;
     String prefixStr = String.valueOf(prefixValue);
     // add extra '0' to the front
@@ -41,7 +41,7 @@ public class DataImportingMapper
 
     String[] fields = line.split("\t");
     if (fields.length != 4) {
-      context.getCounter("BadRecords", "BAD_FIELDS_NUM").increment(1);
+      context.getCounter("Bad Records", "BAD_FIELDS_NUM").increment(1);
       return;
     }
 
@@ -58,41 +58,33 @@ public class DataImportingMapper
     try {
       // check whether values of latitude and longitude fields are valid double
       double longitude = Double.parseDouble(rawLongitude);
-      double latitude = Double.parseDouble(rawLongitude);
+      double latitude = Double.parseDouble(rawLatitude);
+
       // It's impossible that a double value exactly equals to 0.0
       if (longitude == 0.0 || longitude < -180.0 || longitude > 180.0
-          || latitude == 0.0 || latitude < -90.0 || longitude > 90.0) {
+          || latitude == 0.0 || latitude < -90.0 || latitude > 90.0) {
         context.getCounter("Bad Records", "MISSING_FIELD_VALUE").increment(1);
         return;
       }
 
-      normalizedLongitude = normalize(longitude);
-      normalizedLatitude = normalize(latitude);
-
-      Date date = new Date(timestamp);
+      normalizedLongitude = normalize(longitude, -180.0);
+      normalizedLatitude = normalize(latitude, -90.0);
+      long unixTimestamp = Long.parseLong(timestamp);
+      Date date = new Date(unixTimestamp);
       time = sdf.format(date);
-
       GeoHash geohash = GeoHash.withCharacterPrecision(latitude, longitude, 12);
-      rowKey = geohash + time;
+      rowKey = geohash.toBase32() + time;
     } catch (Exception e) {
-      context.getCounter("Bad Records", "BAD_DATA_FORMAT").increment(1);
+      context.getCounter("Bad Records", "BAD_DATA_FORMAT:" + e).increment(1);
       return;
     }
 
-    hKey.set(rowKey.getBytes());
+    Put p = new Put(Bytes.toBytes(rowKey));
 
-    kv = new KeyValue(hKey.get(), COL_FAMILY, HColumnEnum.COL_X.getColumnName(), normalizedLongitude.getBytes());
-    context.write(hKey, kv);
-
-    kv = new KeyValue(hKey.get(), COL_FAMILY, HColumnEnum.COL_Y.getColumnName(), normalizedLatitude.getBytes());
-    context.write(hKey, kv);
-
-    kv = new KeyValue(hKey.get(), COL_FAMILY, HColumnEnum.COL_T.getColumnName(), time.getBytes());
-    context.write(hKey, kv);
-
-    if (description.length() != 0) {
-      kv = new KeyValue(hKey.get(), COL_FAMILY, HColumnEnum.COL_D.getColumnName(), description.getBytes());
-      context.write(hKey, kv);
-    }
+    p.addColumn(EVENT_COLUMNFAMILY, LONGITUDE_QUALIFIER, Bytes.toBytes(normalizedLongitude));
+    p.addColumn(EVENT_COLUMNFAMILY, LATITUDE_QUALIFIER, Bytes.toBytes(normalizedLatitude));
+    p.addColumn(EVENT_COLUMNFAMILY, TIME_QUALIFIER, Bytes.toBytes(time));
+    p.addColumn(EVENT_COLUMNFAMILY, DESCRIPTION_QUALIFIER, Bytes.toBytes(description));
+    context.write(null, p);
   }
 }
